d92c505255e3e64f3bff9bd76c848ec30942049f
peiht <peiht@yonyou.com>
2018-03-19 13:29:32 +0800

    注释

--- learning/src/learning/classloader/ClassLoaderTest.java
+++ learning/src/learning/classloader/ClassLoaderTest.java
@@ -1,5 +1,12 @@
 package learning.classloader;
 
+/**
+ * 类加载的步骤：加载、验证、准备、解析、初始化、使用、卸载。
+ * @ClassName: ClassLoaderTest
+ * @Description: TODO(类简要描述，必须以句号为结束)
+ * @author Administrator
+ * @date 2018年3月19日
+ */
 public class ClassLoaderTest {
 
 	public static void main(String[] args) {
