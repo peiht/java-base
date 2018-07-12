65a9da1bb04a93b8f5754bb091ecf14c65126d01
peiht <1071532941@qq.com>
2018-07-12 23:50:32 +0800

    update

---
+++ learning/src/collection/CollectionData.java
@@ -0,0 +1,14 @@
+package collection;
+
+
+import java.util.*;
+
+public class CollectionData<T> extends ArrayList<T> {
+
+    /*public CollectionData<T>(Generator<T> generator,int quality){
+
+        for (int i=0;i<quality;i++){
+            add(generator);
+        }
+    }*/
+}
